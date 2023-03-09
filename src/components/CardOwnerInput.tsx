a41d7a2b099a4d8f4e4324636f448f6c20b98a2a
이국신 <nw4611@naver.com>
2023-03-09 17:50:08 +0900

    feature: 카드 소유자 정보 input 컴포넌트 개발

---
+++ src/components/CardOwnerInput.tsx
@@ -0,0 +1,12 @@
+import CardInput from './CardInput'
+
+function CardOwnerInput() {
+    return (
+        <CardInput>
+            <CardInput.Title title={'카드 소유자 이름(선택)'} />
+            <CardInput.Input />
+        </CardInput>
+    )
+}
+
+export default CardOwnerInput
