04d1ab47b8b841fc58c6dfc115a5f59b4fc18e9b
이국신 <nw4611@naver.com>
2023-03-09 17:26:24 +0900

    feat: 카드 모양의 컴포넌트 구현

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className="card-box">
+        <div className="empty-card">
+            <div className="card-top"></div>
+            <div className="card-middle">
+                <div className="small-card__chip"></div>
+            </div>
+            <div className="card-bottom">
+                <div className="card-bottom__info">
+                    <span className="card-text">NAME</span>
+                    <span className="card-text">MM / YY</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
