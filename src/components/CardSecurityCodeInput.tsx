21ef502e86322a81ab683ab107b8f300badd7cda
이국신 <nw4611@naver.com>
2023-03-09 17:51:18 +0900

    feat: 보안코드를 입력받는 컴포넌트 개발

---
+++ src/components/CardSecurityCodeInput.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardInput from './CardInput'
+
+function CardSecurityCodeInput() {
+  return (
+      <CardInput>
+          <CardInput.Title title={'보안코드(CVC/CVV)'} />
+          <CardInput.Input size='w-25'/>
+    </CardInput>
+  )
+}
+
+export default CardSecurityCodeInput
